b827aa496f7036812f9d217de3afeb9c55d637d0
dvanetten03 <diane.vanetten@me.com>
2016-02-15 21:45:07 -0600

    Added brand loop

--- lib/app.rb
+++ lib/app.rb
@@ -4,7 +4,8 @@ file = File.read(path)
 products_hash = JSON.parse(file)
 
 # Print today's date
-puts Time.now.strftime("%m/%d/%Y")
+puts ""
+puts "Today's Date: " + Time.now.strftime("%m/%d/%Y")
 
 puts "                     _            _       "
 puts "                    | |          | |      "
@@ -39,7 +40,7 @@ products_hash["items"].each do |toy|
 
   # Calculate and print the average discount (% or $) based off the average sales price
   avg_discount = ((toy["full-price"].to_f - avg_price.to_f ) / toy["full-price"].to_f) * 100
-  puts "Average Discount: %#{avg_discount.round(2)}"
+  puts "Average Discount: #{avg_discount.round(2)}%"
   puts ""
 
 end
@@ -54,8 +55,14 @@ end
 
 # For each brand in the data set:
 unique_brands = products_hash["items"].map {|item| item["brand"]}.uniq
-puts unique_brands
+  unique_brands.each do |brand|
   # Print the name of the brand
+    puts unique_brands
   # Count and print the number of the brand's toys we stock
+    puts "In stock: " 
+  
   # Calculate and print the average price of the brand's toys
+    puts "Average Price of toys: " 
   # Calculate and print the total revenue of all the brand's toy sales combined
+
+end
